103582e3a6757e163a78616030617d7875ae39e5
Breno Chiaradia <bcorreia629@gmail.com>
2026-05-03 16:59:36 -0300

    Primeiro up

---
+++ script.js
@@ -0,0 +1,9 @@
+function login() {
+    alert("Ir para tela de login");
+    // window.location.href = "login.html";
+}
+
+function cadastrar() {
+    alert("Ir para tela de cadastro");
+    // window.location.href = "cadastro.html";
+}
