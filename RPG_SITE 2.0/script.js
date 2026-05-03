23d5fdca29f1eb709d2f8f1d83f108fa9eacd41b
Breno Chiaradia <bcorreia629@gmail.com>
2026-05-03 17:26:58 -0300

    Delete RPG_SITE 2.0 directory

--- RPG_SITE 2.0/script.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    alert("Ir para tela de login");
-    // window.location.href = "login.html";
-}
-
-function cadastrar() {
-    alert("Ir para tela de cadastro");
-    // window.location.href = "cadastro.html";
-}
